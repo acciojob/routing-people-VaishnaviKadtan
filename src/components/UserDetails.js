baf40d9cc1af71dd5cfa3aa3c40ccb2b39753dc9
VaishnaviKadtan <deepikakadtan15@gmail.com>
2026-06-08 20:50:13 +0530

    Create UserDetails.js

---
+++ src/components/UserDetails.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import UserList from "./UserList";
+import UserDetails from "./UserDetails";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<UserList />} />
+        <Route path="/users/:id" element={<UserDetails />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
